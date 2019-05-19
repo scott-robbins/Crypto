1cd90920e3a82db55c4052a8966c2f2456e09c74
TylersDurden <srobbin1@ithaca.edu>
2019-05-19 14:52:31 -0400

    update

--- utilities/blocks.py
+++ utilities/blocks.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import scipy.io as io
 import numpy as np
+import utils
 import sys
 import os
 
@@ -58,7 +59,7 @@ def process_text(text_data, show):
 
 
 def main():
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1 and '-dims' not in sys.argv:
         fname = sys.argv[1]
         try:
             ext = sys.argv[1].split('.')[1]
@@ -71,14 +72,85 @@ def main():
         if ext == 'txt':
             data = swap(fname, False)
             image_data = process_text(data, False)
-            plt.imsave('data.png', image_data)
+            plt.imsave(fname.split('.')[0]+'.png', image_data)
         if ext == 'pdf':
             image_data = process_text(swap(fname, False), True)
-            plt.imsave('data.png', image_data)
+            plt.imsave(fname.split('.')[0] + '.png', image_data)
         if ext == 'wav':
             data = load_wav(fname)
         if ext == 'mat':
             data = load_mat(fname)
+        # Show the image
+        plt.imshow(image_data, 'gray')
+        plt.show()
+    elif len(sys.argv) >=2 and '-dims' in sys.argv:
+        fname = sys.argv[1]
+        try:
+            ext = sys.argv[1].split('.')[1]
+            if ext in types:
+                print "Processing " + ext
+            sys.argv.remove(fname)
+            sys.argv.pop(0)
+            sys.argv.pop(0)
+        except IndexError:
+            print "Incorrect Syntax!"
+            exit(0)
+        dims = [int(sys.argv[0]), int(sys.argv[1])]
+        state = np.zeros(dims)
+        # Handle various file types and get all the data
+        if ext == 'txt':
+            data = swap(fname, False)
+            test = process_text(data, False)
+        if ext == 'pdf':
+            data = swap(fname, False)
+            test = process_text(data, False)
+
+        if ext == 'wav':
+            data = load_wav(fname)
+        if ext == 'mat':
+            data = load_mat(fname)
+
+        cx = dims[0]/2
+        cy = dims[1]/2
+
+        padx = dims[0] - test.shape[0]
+        pady = dims[1] - test.shape[1]
+
+        state[cx-test.shape[0]/2:cx+test.shape[0]/2,
+              cy-test.shape[1]/2:cy+test.shape[1]/2] = test
+
+        print 'x_pad: ' + str(padx) + '\ty_pad: ' + str(pady)
+        print 'Min: ' + str(state.min())
+        print 'Max: ' + str(state.max())
+
+        register = np.zeros((pady/2, padx/2))
+        p = int(np.sqrt(padx*pady/4))
+        if len(register.flatten()) < (state.max()-state.min()) and register.shape[0]-state.shape[0]>p and register.shape[0]-state.shape[1]>p :
+            print 'Illegal Bitmapping!'
+            exit(0)
+        # counter = 0
+        # for ii in range(len(register.flatten())):
+        #     [x,y] = utils.ind2sub(ii,register.shape)
+        #     register[x-5:x+10,y-5:y+10] = counter
+        #     counter += 1
+        #     if counter >= (state.max()-state.min()):
+        #         counter = 0
+        # register = register.reshape((pady/2,padx/2))
+        gscale = np.arange(state.min(), state.max(), 1)
+        for col in range(int(state.max()-state.min())):
+            register[col-10:col,:] = gscale[col]
+        print register.shape
+        # Add Left Register
+        state[p:pady/2+p,p:padx/2+p] = np.rot90(np.rot90(register))
+        # Add Right Register (Rotate 180 Degrees)
+        state[state.shape[0]-pady/2-p:state.shape[0]-p,
+              state.shape[1]-padx/2-p:state.shape[1]-p] = register
+
+        plt.imshow(state, 'gray')
+        plt.show()
+
+        if 'save' in sys.argv:
+            plt.imsave(fname.split('.')[0]+'.png', state)
 
 
 if __name__ == '__main__':
--- utilities/utils.py
+++ utilities/utils.py
@@ -8,3 +8,19 @@ def swap(fname, destroy):
     if destroy:
         os.remove(fname)
     return data
+
+
+def ind2sub(index,dims):
+    """
+    Given an index and array dimensions,
+    convert an index to [x,y] subscript pair.
+    :param index:
+    :param dims:
+    :return tuple - subscripts :
+    """
+    ii = 0
+    for y in range(dims[1]):
+        for x in range(dims[0]):
+            if index==ii:
+                return [x,y]
+            ii +=1
